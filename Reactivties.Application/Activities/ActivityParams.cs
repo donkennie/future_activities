c5cf9b9c8360ba2a3d7f78f81a7ade2950658f69
donkennie <ajeigbekehinde160@gmail.com>
2022-07-23 18:01:50 +0100

    untracked changes

---
+++ Reactivties.Application/Activities/ActivityParams.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reactivties.Application.Activities
+{
+    internal class ActivityParams
+    {
+    }
+}
